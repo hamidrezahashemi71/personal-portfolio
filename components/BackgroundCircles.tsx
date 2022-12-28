ce3f485e8e84161c90579536e5b9c896b5f1ef36
hamidrezahashemi71 <hh7192@gmail.com>
2022-12-28 18:31:52 +0330

    hero section basics

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {motion} from "framer-motion";
+
+type Props = {};
+
+const BackgroundCircles = (props: Props) => {
+  return (
+    <motion.div
+      initial={{opacity: 0}}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 0.3],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+      transition={{duration: 2.5}}
+      className='relative flex justify-center items-center'>
+      {/* <div className='absolute border border-borders rounded-full w-[200px] h-[200px] mt-52 animate-ping ' /> */}
+      <div className='absolute border border-borders rounded-full w-[300px] h-[300px] mt-52' />
+      <div className='absolute border border-borders rounded-full w-[500px] h-[500px] mt-52' />
+      <div className='absolute rounded-full border-orangish opacity-80 w-[650px] h-[650px] mt-52 animate-pulse' />
+      <div className='absolute rounded-full border-borders opacity-20 w-[800px] h-[800px] mt-52' />
+    </motion.div>
+  );
+};
+
+export default BackgroundCircles;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#0A2647",
+      secondary: "#FFF3DF",
+      borders: "#2C74B3",
+      grayish: "#888888",
+      blueish: "#3B5998",
+      reddish: "#FF0000",
+      orangish: "#F7AB0A",
+      red: colors.red,
+      blue: colors.blue,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "Sans-serif"],
+      Code: ["Source Code Pro", "Sans-serif"],
+    },
     extend: {},
   },
+
   plugins: [],
 };
